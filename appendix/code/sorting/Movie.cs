db5bd720d10c0b7c2edeafa05da7d3cf493bd58e
Nemeczek <nemeczekes@gmail.com>
2015-12-18 10:11:42 +0100

    removed tabs and added java brackets notation

--- appendix/code/sorting/Movie.cs
+++ appendix/code/sorting/Movie.cs
@@ -1,20 +1,19 @@
 ﻿namespace SAD2.GeneralApproach
 {
-	public class Movie
+public class Movie
+{
+	public Movie(long id, decimal rating)
 	{
-		public Movie(long id, decimal rating)
-		{
-			Id = id;
-			Rating = rating;
-		}
-
-		public long Id { get; set; }
-		public long Occurences { get; set; }
-		public decimal Rating { get; set; }
-
-		public override string ToString()
-		{
-			return $"{Id},{Rating/Occurences}";
-		}
+		Id = id;
+		Rating = rating;
 	}
-}
+
+	public long Id { get; set; }
+	public long Occurences { get; set; }
+	public decimal Rating { get; set; }
+
+	public override string ToString()
+	{
+		return $"{Id},{Rating/Occurences}";
+	}
+}}
--- appendix/code/sorting/kendalltau/KendallTau.cs
+++ appendix/code/sorting/kendalltau/KendallTau.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+/******************************************************************************
+ *  Based on http://algs4.cs.princeton.edu/25applications/KendallTau.java.html
+ *  Copyright © 2002–2014 Robert Sedgewick and Kevin Wayne. All rights reserved.
+ *  
+ ******************************************************************************/
+namespace SAD2.KendallTauDistance{
+public static class KendallTau{
+public static decimal Distance(decimal[] a, decimal[] b){
+	if (a.Length != b.Length)
+		throw new ArgumentException("Array dimensions disagree");
 
-namespace SAD2.KendallTauDistance
-{
-	public static class KendallTau
+	long n = a.Length;
+	Dictionary<decimal, int> ainvD = new Dictionary<decimal, int>();
+
+	for (int i = 0; i < n; i++)
 	{
-		public static decimal Distance(decimal[] a, decimal[] b)
-		{
-			if (a.Length != b.Length)
-				throw new ArgumentException("Array dimensions disagree");
-
-			long n = a.Length;
-			Dictionary<decimal, int> ainvD = new Dictionary<decimal, int>();
-
-			for (int i = 0; i < n; i++)
-			{
-				if(!ainvD.ContainsKey(a[i]))
-				ainvD.Add(a[i], i);
-			}
-				
-
-			int[] bnew = new int[n];
-			for (int i = 0; i < n; i++)
-			{
-				if(ainvD.ContainsKey(b[i]))
-					bnew[i] = ainvD[b[i]];
-			}
-
-			var inversions = Inversions.count(bnew);
-			decimal bottom = (n*(n - 1))/2.0M;
-			
-			return Math.Round(inversions / bottom, 2); ;
-		}
+		if(!ainvD.ContainsKey(a[i]))
+		ainvD.Add(a[i], i);
 	}
-}
+		
+
+	int[] bnew = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		if(ainvD.ContainsKey(b[i]))
+			bnew[i] = ainvD[b[i]];
+	}
+
+	var inversions = Inversions.count(bnew);
+	decimal bottom = (n*(n - 1))/2.0M;
+	
+	return Math.Round(inversions / bottom, 2); ;}
+}}
